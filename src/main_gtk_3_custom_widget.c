e5715557590834a35328891f61ce4222c8b6af1e
Yogi Astawan <yogi.astawan@gmail.com>
2020-08-11 14:08:46 +0800

    include gisacompass.h

--- src/main_gtk_3_custom_widget.c
+++ src/main_gtk_3_custom_widget.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 
 #include "./customWidget/gisagauge.h"
+#include "./customWidget/gisacompass.h"
 
 void increaseValue(GtkWidget *widget,gpointer *data);
 void decreaseValue(GtkWidget *widget,gpointer *data);
@@ -25,13 +26,15 @@ void main_ui(GtkApplication *app, gpointer *user_data)
 	gtk_window_set_title(GTK_WINDOW(window), (gchar*)user_data);
 
 	GtkWidget *button, *button2, *button_box, *box;
-	GtkWidget *myGauge;	
+	GtkWidget *myGauge;
+	GtkWidget *myCompass;
 
 	button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
 	box=gtk_box_new(GTK_ORIENTATION_VERTICAL,2);
 	gtk_container_add(GTK_CONTAINER(window), box);
 
 	myGauge=gisa_gauge_new();
+	myCompass=gisa_compass_new();
 
 	button = gtk_button_new_with_label("Increase");
 	g_signal_connect(button, "clicked", G_CALLBACK(increaseValue), myGauge);
@@ -42,6 +45,7 @@ void main_ui(GtkApplication *app, gpointer *user_data)
 	gtk_container_add(GTK_CONTAINER(button_box), button2);
 	
 	gtk_box_pack_start(GTK_BOX(box),myGauge,FALSE,TRUE,0);
+	gtk_box_pack_start(GTK_BOX(box),myCompass,FALSE,TRUE,0);
 	gtk_box_pack_start(GTK_BOX(box),button_box,FALSE,FALSE,0);
 
 	gtk_widget_show_all(window);

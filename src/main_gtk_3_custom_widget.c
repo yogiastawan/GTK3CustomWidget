d0512a4f884560432fe436c93086968fd608a085
Yogi Astawan <yogi.astawan@gmail.com>
2020-08-16 00:25:26 +0800

    add style css

--- src/main_gtk_3_custom_widget.c
+++ src/main_gtk_3_custom_widget.c
@@ -23,36 +23,43 @@ void main_ui(GtkApplication *app, gpointer *user_data)
 {
 	GtkWidget *window;
 	window = gtk_application_window_new(app);
-	gtk_window_set_title(GTK_WINDOW(window), (gchar*)user_data);
+	gtk_window_set_title(GTK_WINDOW(window), (gchar *)user_data);
 
 	GtkWidget *slider1, *slider2, *slider_box, *box, *boxh;
 	GtkWidget *myGauge;
 	GtkWidget *myCompass;
 
 	slider_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
-	box=gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
 	gtk_container_add(GTK_CONTAINER(window), box);
 
-	boxh=gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
+	boxh = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
 
-	myGauge=gisa_gauge_new();
-	myCompass=gisa_compass_new();
+	myGauge = gisa_gauge_new();
+	myCompass = gisa_compass_new();
 	gtk_box_pack_start(GTK_BOX(box), boxh, TRUE, TRUE, 0);
 
-	GtkAdjustment *scale1Adj=gtk_adjustment_new(0, 0, 100, 1, 5, 0);
-	slider1=gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, scale1Adj);
+	GtkAdjustment *scale1Adj = gtk_adjustment_new(0, 0, 100, 1, 5, 0);
+	slider1 = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, scale1Adj);
 	g_signal_connect(slider1, "value-changed", G_CALLBACK(gaugeTest), myGauge);
 	// gtk_container_add(GTK_CONTAINER(slider_box), slider1);
 	gtk_box_pack_start(GTK_BOX(slider_box), slider1, FALSE, TRUE, 0);
 
-	GtkAdjustment *scale2Adj=gtk_adjustment_new(0, -360, 360, 1, 5, 0);
-	slider2=gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, scale2Adj);
+	GtkAdjustment *scale2Adj = gtk_adjustment_new(0, -360, 360, 1, 5, 0);
+	slider2 = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, scale2Adj);
 	g_signal_connect(slider2, "value-changed", G_CALLBACK(compassTest), myCompass);
 	gtk_box_pack_start(GTK_BOX(slider_box), slider2, FALSE, TRUE, 0);
 
 	gtk_box_pack_start(GTK_BOX(boxh), myGauge, FALSE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(boxh), myCompass, FALSE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(box), slider_box, TRUE, TRUE, 0);
+
+	//style
+	GtkCssProvider *provider = gtk_css_provider_new();
+	gtk_css_provider_load_from_path(provider, "/home/yogiastawan/Programming/C/GTK3CustomWidget/res/style/style.css", NULL);
+	GtkStyleContext *ctx;
+	ctx = gtk_widget_get_style_context(myGauge);
+	gtk_style_context_add_provider(ctx, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
 	gtk_widget_show_all(window);
 }
